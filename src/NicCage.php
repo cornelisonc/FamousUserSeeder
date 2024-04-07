f356381dd8377fa1b7bdc90b476252c3d3af3273
Ceili Cornelison <ccornelison@deltasys.com>
2024-04-07 13:02:35 -0500

    NicCage data class

---
+++ src/NicCage.php
@@ -0,0 +1,318 @@
+<?php
+
+namespace Cornelisonc\FamousUserSeeder;
+
+class NicCage extends FamousUserSeeder
+{
+    private $data = [
+        [
+            'first' => 'Vincent',
+            'last' => 'Dwyer'
+        ],
+        [
+            'first' => 'Al',
+            'last' => 'Columbato'
+        ],
+        [
+            'first' => 'Ned',
+            'last' => 'Hanlan'
+        ],
+        [
+            'first' => 'Charlie',
+            'last' => 'Bodell'
+        ],
+        [
+            'first' => 'H.I.',
+            'last' => 'McDunnough'
+        ],
+        [
+            'first' => 'Ronny',
+            'last' => 'Cammareri'
+        ],
+        [
+            'first' => 'Peter',
+            'last' => 'Loew'
+        ],
+        [
+            'first' => 'Enrico',
+            'last' => 'Silvestri'
+        ],
+        [
+            'first' => 'Jake',
+            'last' => 'Preston'
+        ],
+        [
+            'first' => 'Johnny',
+            'last' => 'Collins'
+        ],
+        [
+            'first' => 'Jack',
+            'last' => 'Singer'
+        ],
+        [
+            'first' => 'Amos',
+            'last' => 'Odell'
+        ],
+        [
+            'first' => 'Doug',
+            'last' => 'Chesnic'
+        ],
+        [
+            'first' => 'Charlie',
+            'last' => 'Lang'
+        ],
+        [
+            'first' => 'Bill',
+            'last' => 'Firpo'
+        ],
+        [
+            'first' => 'Little Junior',
+            'last' => 'Brown'
+        ],
+        [
+            'first' => 'Ben',
+            'last' => 'Sanderson'
+        ],
+        [
+            'first' => 'Stanley',
+            'last' => 'Goodspeed'
+        ],
+        [
+            'first' => 'Cameron',
+            'last' => 'Poe'
+        ],
+        [
+            'first' => 'Castor',
+            'last' => 'Troy'
+        ],
+        [
+            'first' => 'Rick',
+            'last' => 'Santoro'
+        ],
+        [
+            'first' => 'Tom',
+            'last' => 'Welles'
+        ],
+        [
+            'first' => 'Frank',
+            'last' => 'Pierce'
+        ],
+        [
+            'first' => 'Memphis',
+            'last' => 'Raines'
+        ],
+        [
+            'first' => 'Jack',
+            'last' => 'Campbell'
+        ],
+        [
+            'first' => 'Antonio',
+            'last' => 'Corelli'
+        ],
+        [
+            'first' => 'Jacob',
+            'last' => 'Marley'
+        ],
+        [
+            'first' => 'Joe',
+            'last' => 'Enders'
+        ],
+        [
+            'first' => 'Acid',
+            'last' => 'Yellow'
+        ],
+        [
+            'first' => 'Charlie',
+            'last' => 'Kaufman'
+        ],
+        [
+            'first' => 'Donald',
+            'last' => 'Kaufman'
+        ],
+        [
+            'first' => 'Roy',
+            'last' => 'Waller'
+        ],
+        [
+            'first' => 'Benjamin Franklin',
+            'last' => 'Gates'
+        ],
+        [
+            'first' => 'Yuri',
+            'last' => 'Orlov'
+        ],
+        [
+            'first' => 'David',
+            'last' => 'Spritz'
+        ],
+        [
+            'first' => 'John',
+            'last' => 'McLoughlin'
+        ],
+        [
+            'first' => 'Edward',
+            'last' => 'Malus'
+        ],
+        [
+            'first' => 'Johnny',
+            'last' => 'Blaze'
+        ],
+        [
+            'first' => 'Ghost',
+            'last' => 'Rider'
+        ],
+        [
+            'first' => 'Fu',
+            'last' => 'Manchu'
+        ],
+        [
+            'first' => 'Cris',
+            'last' => 'Johnson'
+        ],
+        [
+            'first' => 'John',
+            'last' => 'Koestler'
+        ],
+        [
+            'first' => 'Terence',
+            'last' => 'McDonagh'
+        ],
+        [
+            'first' => 'Dr.',
+            'last' => 'Tenma'
+        ],
+        [
+            'first' => 'Damon',
+            'last' => 'Macready'
+        ],
+        [
+            'first' => 'Big',
+            'last' => 'Daddy'
+        ],
+        [
+            'first' => 'Will',
+            'last' => 'Gerard'
+        ],
+        [
+            'first' => 'Kyle',
+            'last' => 'Miller'
+        ],
+        [
+            'first' => 'Will',
+            'last' => 'Montgomery'
+        ],
+        [
+            'first' => 'Grug',
+            'last' => 'Crood'
+        ],
+        [
+            'first' => 'Jack',
+            'last' => 'Halcombe'
+        ],
+        [
+            'first' => 'Paul',
+            'last' => 'Maguire'
+        ],
+        [
+            'first' => 'Rayford',
+            'last' => 'Steele'
+        ],
+        [
+            'first' => 'Evan',
+            'last' => 'Lake'
+        ],
+        [
+            'first' => 'Colin',
+            'last' => 'Pryce'
+        ],
+        [
+            'first' => 'Hank',
+            'last' => 'Forrester'
+        ],
+        [
+            'first' => 'Captain',
+            'last' => 'McVay'
+        ],
+        [
+            'first' => 'Gary',
+            'last' => 'Faulkner'
+        ],
+        [
+            'first' => 'Eddie',
+            'last' => 'King'
+        ],
+        [
+            'first' => 'Red',
+            'last' => 'Miller'
+        ],
+        [
+            'first' => 'Noah',
+            'last' => 'Kross'
+        ],
+        [
+            'first' => 'Mike',
+            'last' => 'Chandler'
+        ],
+        [
+            'first' => 'Kal',
+            'last' => 'El'
+        ],
+        [
+            'first' => 'Peter',
+            'last' => 'Parker'
+        ],
+        [
+            'first' => 'Frank',
+            'last' => 'Carver'
+        ],
+        [
+            'first' => 'The',
+            'last' => 'Cook'
+        ],
+        [
+            'first' => 'Frank',
+            'last' => 'Walsh'
+        ],
+        [
+            'first' => 'Grug',
+            'last' => 'Crood'
+        ],
+        [
+            'first' => 'The',
+            'last' => 'Janitor'
+        ],
+        [
+            'first' => 'Rob',
+            'last' => 'Feld'
+        ],
+        [
+            'first' => 'Colton',
+            'last' => 'Briggs'
+        ],
+        [
+            'first' => 'Paul',
+            'last' => 'Matthews'
+        ],
+        [
+            'first' => 'Nicolas',
+            'last' => 'Cage'
+        ],
+    ];
+
+    public function __construct($quantity = 1)
+    {
+        $out        = [];
+
+        while (sizeof($out) < $quantity) {
+            $shuffled   = shuffle($this->data);
+
+            if (sizeof($shuffled) >= $quantity) {
+                $out = array_slice($shuffled, 0, $quantity);
+            } else {
+                $out = array_push($out, $shuffled);
+            }
+        }
+
+        return $out;
+    }
+}
