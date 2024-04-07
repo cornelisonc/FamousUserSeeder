b2ea03ce52ca889e64f2b9fdceb8449847bbc4cf
Ceili Cornelison <ccornelison@deltasys.com>
2024-04-07 13:02:51 -0500

    FamousUsers abstract class, needs concrete famous implementation

--- src/FamousUserSeeder.php
+++ src/FamousUserSeeder.php
@@ -2,10 +2,27 @@
 
 namespace Cornelisonc\FamousUserSeeder;
 
-class FamousUserSeeder
+abstract class FamousUserSeeder
 {
+    private $data;
+
     public function __construct()
     {
         echo 'FamousUserSeeder';
+    }
+
+    public function firstName($node)
+    {
+        return $node['first'];
+    }
+
+    public function lastName($node)
+    {
+        return $node['last'];
+    }
+
+    public function fullName($node)
+    {
+        return $node['first'] . ' ' . $node['last'];
     }
 }
